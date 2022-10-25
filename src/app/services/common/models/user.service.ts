6fdf74fb988444f6f814fb832da185314335223b
Emre Başpınar <emre-baspinar1605@outlook.com>
2022-10-25 15:29:27 +0300

    25.10.2022

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
 import { create_user } from 'src/app/contracts/users/create_user';
+import { Token } from 'src/app/contracts/users/token/token';
+import { TokenResponse } from 'src/app/contracts/users/token/TokenResponse';
 import { User } from 'src/app/entities/user';
+import { MessageType, Position } from '../../admin/alertify.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -9,7 +13,7 @@ import { HttpClientService } from '../http-client.service';
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService, private toastrService: CustomToastrService) { }
   
   async create(user: User): Promise<create_user>{
    const observable: Observable<create_user | User> = this.httpClientService.post<create_user | User>({
@@ -17,13 +21,24 @@ export class UserService {
     }, user);
     return await firstValueFrom(observable) as create_user;
   }
-  async login(usernameOrEmail: string, password: string, callBackFunction?: () => void): Promise<void>
+  async login(usernameOrEmail: string, password: string, callBackFunction?: () => void): Promise<any>
   {
-   const  observable: Observable<any> = this.httpClientService.post({
+   const  observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller: "users",
       action:"login"
     }, {usernameOrEmail, password});
-    await firstValueFrom(observable);
+
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if(tokenResponse)
+    {
+      localStorage.setItem("accessToken",tokenResponse.token.accessToken)
+      this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır.","Giriş Başarılı",
+      {
+        messageType: ToastrMessageType.Success,
+        position:ToastrPosition.TopRight
+      });
+    }
+  
     callBackFunction();
   }
 }
